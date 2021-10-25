da8c1a47511a281c0f46eee1d99f7a77e804eda5
Le Van Chien <levanchien.95.cit@gmail.com>
2021-10-25 17:05:48 +0700

    Add 4.2 v2

---
+++ chap-04/4.2/indexV2.mjs
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+function listNestedFiles(dir, callback) {
+  let result = [];
+  fs.readdir(dir, (rdErr, files) => {
+    if (rdErr) {
+      return callback(rdErr);
+    }
+
+    if (files.length === 0) {
+      return process.nextTick(() => callback(null, result));
+    }
+
+    let length = 0;
+
+    /* index loop quá nhanh dẫn đến  khi check thì index luôn === files.length */
+    for (let index = 0; index < files.length; index++) {
+      const path = `${dir}/${files[index]}`;
+      fs.stat(path, (sErr, stat) => {
+        if (sErr) {
+          return callback(sErr);
+        }
+        if (stat.isFile()) {
+          result.push(path);
+          if (++length === files.length) {
+            return callback(null, result);
+          }
+        }
+        if (stat.isDirectory()) {
+          listNestedFiles(path, (e, r) => {
+            if (e) {
+              return callback(e);
+            }
+            result = result.concat(r);
+            if (++length === files.length) {
+              return callback(null, result);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+listNestedFiles("..", (e, r) => {
+  if (e) {
+    throw new Error(e);
+  }
+  console.log(r);
+});
