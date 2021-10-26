07c6d100d9adc6b085e96bbe3ab2fa0dda838bf2
Le Van Chien <levanchien.95.cit@gmail.com>
2021-10-26 10:45:59 +0700

    Add 5.1

---
+++ chap-05/5.1/promiseAll.mjs
@@ -0,0 +1,33 @@
+import { delay } from "./delay.mjs";
+
+function promiseAll(tasks) {
+  const results = new Array(tasks.length);
+  let done = 0;
+  return new Promise((resolve, reject) => {
+    tasks.forEach((task, index) => {
+      task.then(
+        (result) => {
+          results[index] = result;
+          if (++done === tasks.length) {
+            resolve(results);
+          }
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+}
+
+const tasks = [];
+for (let i = 10; i > 0; i--) {
+  tasks.push(delay(i * 1000));
+}
+
+promiseAll(tasks).then(
+  (t) => {
+    console.log(t);
+  },
+  (e) => console.error
+);
