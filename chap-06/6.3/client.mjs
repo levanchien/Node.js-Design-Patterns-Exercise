1bc21811d6abec19fb93ece18f198d677223f97f
Le Van Chien <levanchien.95.cit@gmail.com>
2021-11-03 03:05:51 +0700

    Update 6.4

--- chap-06/6.3/client.mjs
+++ chap-06/6.3/client.mjs
@@ -1,45 +1,41 @@
 import { createReadStream } from "fs";
 import net from "net";
-import { Transform } from "stream";
 import path from "path";
 
 const files = process.argv.slice(2).map((t) => path.basename(t));
 
-function addHeader(str = "") {
-  return new Transform({
-    transform(chunk, ec, cb) {
-      const outBuff = Buffer.alloc(4 + 4 + str.length + chunk.length);
-      outBuff.writeUInt32BE(str.length, 0);
-      Buffer.from(str).copy(outBuff, 4);
-      outBuff.writeUInt32BE(chunk.length, 4 + str.length);
-      chunk.copy(outBuff, 8 + str.length);
-      this.push(outBuff);
-      cb();
-    },
-  });
-}
-
 const socket = net.connect(3000, () => {
-  let done = 0;
+  let uploaded = 0;
   files.forEach((file) => {
-    console.log(file);
-    createReadStream(file)
-      .pipe(addHeader(file))
-      .on("readable", function () {
-        let chunk = null;
-        while ((chunk = this.read()) !== null) {
-          socket.write(chunk);
-        }
+    (function proccess() {
+      const filename = path.basename(file);
+      createReadStream(file, {
+        highWaterMark: 64 * 1024 - 1 - file.length,
       })
-      .on("end", () => {
-        console.log("File uploaded");
-        if (++done === files.length) {
-          console.log("end");
-          socket.end();
-        }
-      })
-      .on("error", (err) => {
-        console.log(err);
-      });
+        .on("readable", function () {
+          let chunk = null;
+          while ((chunk = this.read()) !== null) {
+            const outBuff = Buffer.alloc(1 + filename.length + chunk.length);
+            outBuff.writeUInt8(filename.length, 0);
+            Buffer.from(filename).copy(outBuff, 1);
+            chunk.copy(outBuff, 1 + filename.length);
+            socket.write(outBuff);
+          }
+        })
+        /*
+          incoming data: 64 * 1024 + 1 + file.length = 65537 + file.length
+          the limit of writing to socket - highWaterMark: 64 * 1024 = 65536
+          => backpressure = 65537 + file.length - 65536 = 1 + file.length
+        */
+        .on("end", () => {
+          console.log("Uploaded " + filename);
+          if (++uploaded === files.length) {
+            socket.end();
+          }
+        })
+        .on("error", (err) => {
+          console.log(err);
+        });
+    })();
   });
 });
--- chap-06/6.3/server.mjs
+++ chap-06/6.3/server.mjs
@@ -1,76 +1,40 @@
 import { createWriteStream } from "fs";
 import net from "net";
-import { PassThrough } from "stream";
-
-let streamMap = new Map();
-
-function getWriteStream(filename) {
-  if (!streamMap.get(filename)) {
-    streamMap.set(
-      filename,
-      createWriteStream(filename)
-        .on("close", () => {
-          console.log("Close");
-        })
-        .on("finish", () => {
-          console.log("Finish");
-        })
-    );
-  }
-  return streamMap.get(filename);
-}
-
-function demultiplexFile(source) {
-  let currentFilename = null;
-  let currentFilenameLength = null;
-  let currentDataLength = null;
-  let writeableStream = null;
-  console.log("cakkk");
-  source
-    .pipe(new PassThrough())
-    .on("readable", () => {
-      let chunk;
-      if (currentFilename === null) {
-        chunk = source.read(4);
-        currentFilenameLength = chunk && chunk.readUInt32BE(0);
-        if (!currentFilenameLength) {
-          return null;
-        }
-        currentFilename = source.read(currentFilenameLength).toString();
-        if (writeableStream === null) {
-          writeableStream = getWriteStream(`uploaded/${currentFilename}`);
-        }
-      }
-      if (currentDataLength === null) {
-        chunk = source.read(4);
-        currentDataLength = chunk && chunk.readUInt32BE(0);
-        if (currentDataLength === null) {
-          return null;
-        }
-      }
-
-      chunk = source.read(currentDataLength);
-      if (chunk === null) {
-        return null;
-      }
-
-      console.log(`Received packet from file: ${currentFilename}`);
-      writeableStream.write(chunk);
-
-      currentDataLength = null;
-      currentFilenameLength = null;
-      currentFilename = null;
-      writeableStream = null;
-    })
-    .on("end", () => {
-      streamMap = new Map();
-    })
-    .on("error", (err) => {});
-}
 
 net
   .createServer((socket) => {
-    demultiplexFile(socket);
+    let streamMap = new Map();
+    socket
+      .on("close", () => {
+        console.log("File saved");
+        process.nextTick(() => {
+          streamMap.forEach((writeStream, file) => writeStream.end());
+          streamMap = new Map();
+        });
+      })
+      .on("data", (chunk) => {
+        const filenameLength = chunk.readUInt8(0);
+        const filename = chunk.slice(1, filenameLength + 1).toString();
+        if (!filename) return;
+
+        const data = chunk.slice(1 + filenameLength);
+        if (!data) return;
+
+        let writeStream = streamMap.get(filename);
+        if (!writeStream) {
+          writeStream = createWriteStream(`uploaded/${filename}`).on(
+            "close",
+            () => {
+              console.log("Saved file: " + filename);
+            }
+          );
+          streamMap.set(filename, writeStream);
+        }
+        writeStream.write(data);
+      })
+      .on("error", (err) => {
+        console.log(err);
+      });
   })
   .listen(3000, () => {
     console.log("Server is started");
